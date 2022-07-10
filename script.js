38ec12839eb2bcc404f09bc39419b11d97cc4d51
Essan'Utep Blue <eblue.codes@gmail.com>
2022-07-10 14:31:14 -0700

    Adding a bit more styling to the page. The game logic isn't fully functional yet.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const gameBoard = (function(){
-  const boardArray = ['x','o','x','o','x','o','x','o','x']
+  const boardArray = new Array(9).fill('')
   console.log(boardArray)
   // Link our board element to a variable
   const board = document.getElementById('game-board')
@@ -18,17 +18,62 @@ const gameBoard = (function(){
     }
   }
 
-  const Player = (playerName) => {
-    const playerName = playerName;
-  } 
-
+  const createPlayer = function(name, marker) {
+    return {
+      name: name,
+      marker: marker,
+      moves: [],
+      isWinner: false
+    }
+  }
+  
+  
   return {
     createBoard,
+    createPlayer,
     boardArray: boardArray
   }
 
 })();
 
 
+// Create a module to control the flow of the game
+const displayController = (function(){
+  const form = document.getElementById('form');
+  const submit = document.getElementById('submit')
+  const turnIndicator = document.getElementById('turn-indicator')
+  
+
+  let player1 = gameBoard.createPlayer('', 'X', []);
+  let player2 = gameBoard.createPlayer('', 'O', [])
+
+  submit.addEventListener('click', function(e) {
+    e.preventDefault();
+    // Get player one and two's names using a form
+    player1.name = document.getElementById('player-1').value;
+    player2.name = document.getElementById('player-2').value;
+    // Hide the form after they hit the submit button and start the game!
+    form.classList.add('hidden');
+  })
+  
+  do {
+    turnIndicator.textContent(`Make your move ${player1.name}`)
+    document.querySelectorAll('.space').forEach(space => {
+      space.addEventListener('click', function() {
+        space.textContent = player1.marker
+      })
+    })
+
+
+  } while (player1.isWinner === false && player2.isWinner === false);
+
+  
+
+})()
+
+
 gameBoard.createBoard(gameBoard.boardArray)
+displayController();
+
+
 
